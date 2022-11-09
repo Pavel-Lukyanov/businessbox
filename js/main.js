cebc24c085e47168e3fee92bee94ce0d0b08d6ca
Павел Лукьянов <pahan-01.04.10@mail.ru>
2022-11-09 17:57:06 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -57,12 +57,19 @@ document.addEventListener('DOMContentLoaded', function () {
         accordeonBtns.forEach(el => {
             el.addEventListener('click', function () {
                 el.parentElement.classList.toggle('active');
-                if(el.textContent.trim() == 'Ответ') {
+                if (el.textContent.trim() == 'Ответ') {
                     el.textContent = 'Скрыть ответ';
-                } else if(el.textContent.trim() == 'Скрыть ответ'){
+                } else if (el.textContent.trim() == 'Скрыть ответ') {
                     el.textContent = 'Ответ';
                 }
             })
         })
     }
+
+    //Маска телефона
+    var selector = document.querySelectorAll("input[type='tel']");
+    var im = new Inputmask("+7 (999)-999-99-99");
+
+    im.mask(selector);
+
 })
